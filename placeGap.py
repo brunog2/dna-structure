d67dddcd5536fcca47cc9e46783f9980141d4654
Bruno Gomes <bgbf@ic.ufal.br>
2022-09-19 20:16:36 -0300

    fix: multiple gaps

--- placeGap.py
+++ placeGap.py
@@ -1,14 +1,4 @@
-dna1 = ['a','b','c','d']
-dna2 = ['b','d','a','b']
-dna3 = ['a','d','c','a']
-dna4 = ['d','a','c','b']
-
-#A,C,T,G 
-
-dna = [dna1, dna2,dna3,dna4]
-
-
-def coloca_gap(dna,posicao):
+def coloca_gap(dna, posicao):
     base = dna[posicao]
     dna[posicao] = '-'
     dna.append(0)
@@ -22,7 +12,31 @@ def coloca_gap(dna,posicao):
     return dna
 
 
-for i in range(0,len(dna)):
+dna1 = ['a','c','b','d']
+dna2 = ['d','c','b','a']
+# dna1 = ['a','b','c','d']
+# dna2 = ['b','d','a','b']
+dna3 = ['b','d','c','a']
+dna4 = ['d','a','c','b']
+dna = [dna1, dna2, dna3, dna4]
+
+"""
+a b c d
+a d b c
+b a c d 
+c a d b 
+a c b d
+
+a b c d
+a - - d b c
+- - - - b a c d
+- - - - - - c a d b
+- - - - - - - a c b d
+"""
+
+#A,C,T,G 
+
+for i in range(0, len(dna)):
     try:
         
         dna1 = dna[i]
@@ -35,12 +49,20 @@ for i in range(0,len(dna)):
             base1 = dna1[cont]
             base2 = dna2[cont]
             
-            if(base1 != base2):
+            if (base1 != base2):
+                print(dna1)
+                print(dna2)
+                print('\n')
                 try:
-                    aux = dna1[cont+1]
-                    if(base2 == aux):
-                        #se eu colocar 1 gap, ou seja, mover uma parte do dna para direita, iguala a base
-                        dna2 = coloca_gap(dna2,cont)
+                    jumps = 1
+                    aux = dna1[cont+jumps]
+
+                    while (base2 != aux): 
+                        jumps += 1
+                        aux = dna1[cont+jumps]
+                    #se eu colocar 1 gap, ou seja, mover uma parte do dna para direita, iguala a base
+                    # if (base2 == aux):
+                    dna2 = coloca_gap(dna2, cont)
                 except:
                     pass
             cont += 1
