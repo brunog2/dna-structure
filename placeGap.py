63743198efb3de0065d1a144bc4eebee12457402
Bruno Gomes <bgbf@ic.ufal.br>
2022-09-19 20:40:56 -0300

    add: initial logic for conditional gaps

--- placeGap.py
+++ placeGap.py
@@ -20,6 +20,12 @@ dna3 = ['b','d','c','a']
 dna4 = ['d','a','c','b']
 dna = [dna1, dna2, dna3, dna4]
 
+# 1 - gaps preferencialmente no início
+# 2 - gaps preferencialmente no meio
+# 3 - gaps preferencialmente no fim
+
+way = 1
+
 """
 a b c d
 a d b c
@@ -38,11 +44,10 @@ a - - d b c
 
 for i in range(0, len(dna)):
     try:
-        
         dna1 = dna[i]
         dna2 = dna[i+1]
-
-        cont = 0 
+        middle = int(len(dna1)/2)
+        cont = 0 if way == 1 else middle if way == 2 else -1
         p = len(dna1)
 
         while (cont < p):
@@ -53,16 +58,29 @@ for i in range(0, len(dna)):
                 print(dna1)
                 print(dna2)
                 print('\n')
+
                 try:
                     jumps = 1
                     aux = dna1[cont+jumps]
 
                     while (base2 != aux): 
-                        jumps += 1
+                        # calcular onde colocar os gaps, dependendo do sentido 
+                        # e dos gaps ao redor (se for inserir no meio)
+                        jumps += 1 if way == 1 else -1
+                        middle = int(len(dna2)/2)
+
+                        if way == 2:
+                            firstHalf = dna2[middle:].count('-')
+                            lastHalf = dna2[:len(dna2)-middle].count('-')
+                            
+                            jumps = middle if firstHalf < lastHalf else len(dna2)-middle
+                            
                         aux = dna1[cont+jumps]
                     #se eu colocar 1 gap, ou seja, mover uma parte do dna para direita, iguala a base
                     # if (base2 == aux):
+                
                     dna2 = coloca_gap(dna2, cont)
+                    
                 except:
                     pass
             cont += 1
